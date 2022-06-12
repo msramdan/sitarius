e8c7e5b7dde5566bb942017a330904a04131a29e
muhammad.ramdan <muhammad.ramdan@spesolution.com>
2022-06-12 16:07:48 +0700

    feat: first commit

---
+++ application/helpers/fungsi_helper.php
@@ -0,0 +1,29 @@
+<?php
+date_default_timezone_set('Asia/Jayapura');
+
+function check_already_login()
+{
+	$ci = &get_instance();
+	$user_session = $ci->session->userdata('userid');
+	if ($user_session) {
+		redirect('dashboard');
+	}
+}
+
+function is_login()
+{
+	$ci = &get_instance();
+	$user_session = $ci->session->userdata('userid');
+	if (!$user_session) {
+		redirect('auth');
+	}
+}
+
+function check_admin()
+{
+	$ci = &get_instance();
+	$ci->load->library('fungsi');
+	if ($ci->fungsi->user_login()->level_id != 1) {
+		redirect('dashboard_user');
+	}
+}
---
+++ application/views/dashboard.php
@@ -0,0 +1,254 @@
+<div id="content" class="content">
+	<!-- begin row -->
+	<div class="row">
+		<!-- begin col-3 -->
+		<div class="col-md-3 col-sm-6">
+			<div class="widget widget-stats bg-green">
+				<div class="stats-icon"><i class="fa fa-desktop"></i></div>
+				<div class="stats-info">
+					<h4>DATA PENDUDUK</h4>
+					<p>100</p>
+				</div>
+				<div class="stats-link">
+					<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<!-- end col-3 -->
+		<!-- begin col-3 -->
+		<div class="col-md-3 col-sm-6">
+			<div class="widget widget-stats bg-blue">
+				<div class="stats-icon"><i class="fa fa-chain-broken"></i></div>
+				<div class="stats-info">
+					<h4>JUMLAH LAKI-LAKI</h4>
+					<p>60</p>
+				</div>
+				<div class="stats-link">
+					<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<!-- end col-3 -->
+		<!-- begin col-3 -->
+		<div class="col-md-3 col-sm-6">
+			<div class="widget widget-stats bg-purple">
+				<div class="stats-icon"><i class="fa fa-users"></i></div>
+				<div class="stats-info">
+					<h4>JUMLAH PEREMPUAN</h4>
+					<p>40</p>
+				</div>
+				<div class="stats-link">
+					<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+				</div>
+			</div>
+		</div>
+		<!-- end col-3 -->
+		<!-- begin col-3 -->
+		<div class="col-md-3 col-sm-6">
+			<div class="widget widget-stats bg-red">
+				<div class="stats-icon"><i class="fa fa-clock-o"></i></div>
+				<div class="stats-info">
+					<h4>DATA USER</h4>
+					<p>80</p>
+				</div>
+				<div class="stats-link">
+					<a href="javascript:;">View Detail <i class="fa fa-arrow-circle-o-right"></i></a>
+				</div>
+			</div>
+		</div>
+
+		<script src="https://code.highcharts.com/highcharts.js"></script>
+		<script src="https://code.highcharts.com/modules/exporting.js"></script>
+		<script src="https://code.highcharts.com/modules/export-data.js"></script>
+		<script src="https://code.highcharts.com/modules/accessibility.js"></script>
+		
+		<script src="https://code.highcharts.com/modules/data.js"></script>
+		<script src="https://code.highcharts.com/modules/drilldown.js"></script>
+		
+		
+		
+
+
+		<div class="col-md-6 ui-sortable">
+			<div class="panel panel-inverse" data-sortable-id="index-1">
+				<div class="panel-heading">
+					<div class="panel-heading-btn">
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+					</div>
+					<h4 class="panel-title">Analytics</h4>
+				</div>
+				<div class="panel-body">
+					<div id="container"></div>
+				</div>
+			</div>
+		</div>
+		<div class="col-md-6 ui-sortable">
+			<div class="panel panel-inverse" data-sortable-id="index-1">
+				<div class="panel-heading">
+					<div class="panel-heading-btn">
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-default" data-click="panel-expand"><i class="fa fa-expand"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-success" data-click="panel-reload"><i class="fa fa-repeat"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-warning" data-click="panel-collapse"><i class="fa fa-minus"></i></a>
+						<a href="javascript:;" class="btn btn-xs btn-icon btn-circle btn-danger" data-click="panel-remove"><i class="fa fa-times"></i></a>
+					</div>
+					<h4 class="panel-title">Analytics</h4>
+				</div>
+				<div class="panel-body">
+				<div id="container2"></div>
+				</div>
+			</div>
+		</div>
+
+		<!-- end col-3 -->
+	</div>
+</div>
+
+<script>
+	Highcharts.setOptions({
+		colors: Highcharts.map(Highcharts.getOptions().colors, function(color) {
+			return {
+				radialGradient: {
+					cx: 0.5,
+					cy: 0.3,
+					r: 0.7
+				},
+				stops: [
+					[0, color],
+					[1, Highcharts.color(color).brighten(-0.3).get('rgb')] // darken
+				]
+			};
+		})
+	});
+
+	// Build the chart
+	Highcharts.chart('container2', {
+		chart: {
+			plotBackgroundColor: null,
+			plotBorderWidth: null,
+			plotShadow: false,
+			type: 'pie'
+		},
+		title: {
+			text: 'Berdasarkan Jenis Kelamin'
+		},
+		tooltip: {
+			pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+		},
+		accessibility: {
+			point: {
+				valueSuffix: '%'
+			}
+		},
+		plotOptions: {
+			pie: {
+				allowPointSelect: true,
+				cursor: 'pointer',
+				dataLabels: {
+					enabled: true,
+					format: '<b>{point.name}</b>: {point.percentage:.1f} %',
+					connectorColor: 'silver'
+				}
+			}
+		},
+		series: [{
+			name: 'Share',
+			data: [{
+					name: 'Laki-laki',
+					y: 60
+				},
+				{
+					name: 'Perempuan',
+					y: 40
+				}
+			]
+		}]
+	});
+</script>
+
+<script>
+	// Create the chart
+	Highcharts.chart('container', {
+		chart: {
+			type: 'column'
+		},
+		title: {
+			align: 'left',
+			text: 'Berdasarkan Pekerjaan'
+		},
+		accessibility: {
+			announceNewData: {
+				enabled: true
+			}
+		},
+		xAxis: {
+			type: 'category'
+		},
+		yAxis: {
+			title: {
+				text: 'Jumlah Penduduk'
+			}
+
+		},
+		legend: {
+			enabled: false
+		},
+		plotOptions: {
+			series: {
+				borderWidth: 0,
+				dataLabels: {
+					enabled: true,
+					format: '{point.y:.0f}'
+				}
+			}
+		},
+
+		tooltip: {
+			headerFormat: '<span style="font-size:11px">{series.name}</span><br>',
+			pointFormat: '<span style="color:{point.color}">{point.name}</span>: <b>{point.y:.0f}</b> of total<br/>'
+		},
+
+		series: [{
+			name: "Jumlah Penduduk",
+			colorByPoint: true,
+			data: [{
+					name: "IRT",
+					y: 62,
+					drilldown: null
+				},
+				{
+					name: "Pekerja Swasta",
+					y: 10,
+					drilldown: null
+				},
+				{
+					name: "Pelajar",
+					y: 39,
+					drilldown: null
+				},
+				{
+					name: "PNS",
+					y: 29,
+					drilldown: null
+				},
+				{
+					name: "Petani",
+					y: 12,
+					drilldown: null
+				},
+				{
+					name: "Buruh Harian Lepas",
+					y: 18,
+					drilldown: null
+				},
+				{
+					name: "Ojek Online",
+					y: 7,
+					drilldown: null
+				}
+			]
+		}],
+	});
+</script>
